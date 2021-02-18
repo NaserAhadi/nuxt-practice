2bd85ba95721b24514fc7287fe717f8d4609cab0
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-02-18 17:27:02 +0330

    Last Commit of day

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
-    <h1>Hello, I'm a first Vue/Nuxt app!</h1>
+    <nuxt-link to="/users">Users</nuxt-link>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
@@ -19,16 +19,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
